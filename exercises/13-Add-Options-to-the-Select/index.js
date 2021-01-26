9eb9137ed49451833654990c041db78702169114
leisyarcia <leisyarcia@gmail.com>
2021-01-26 19:02:38 +0000

    corrected # 13

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -1,11 +1,30 @@
+// window.onload = function() {
+// 	console.log("The website was loaded...");
+// 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
+
+// 	// your code here
+// 	countries.map(country => {
+// 		var elem = document.createElement("option");
+// 		elem.innerHTML = country;
+// 		document.querySelector("#mySelect").appendChild(elem);
+// 	});
+// };
+
 window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
-  countries.map(country => {
-		var elem = document.createElement("option");
-		elem.innerHTML = country;
-		document.querySelector("#mySelect").appendChild(elem);
+	// your code here
+	let selection = document.querySelector("#mySelect");
+	selection.addEventListener("change", function(e) {
+		alert(e.target.value);
 	});
+
+	for (let i = 0; i < countries.length; i++) {
+		var newOption = document.createElement("option");
+		newOption.value = countries[i];
+		newOption.innerHTML = countries[i];
+		selection.appendChild(newOption);
+		console.log(newOption);
+	}
 };
