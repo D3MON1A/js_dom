960f0b25cb504c12a0fb79cc2a5c27f5378ec1cf
leisyarcia <leisyarcia@gmail.com>
2021-03-05 02:28:20 +0000

    all working

--- exercises/12-Dynamic-HTML-String/index.js
+++ exercises/12-Dynamic-HTML-String/index.js
@@ -1,2 +1,5 @@
-let auxString = "<p>Hello!</p> <strong>My friend</strong>";
-    document.write(mystring);
+let mystring = "<p>Hello!</p> <strong>My friend,</strong>";
+document.write(mystring);
+let date = new Date();
+var year = date.getFullYear();
+document.write(" we are in the year " + year);
